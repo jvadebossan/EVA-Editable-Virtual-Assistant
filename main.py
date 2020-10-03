3fbc0ce29e2752818efed89537174daec1cbbecf
João Vitor de Aguiar Debossan <jvdebossan@gmail.com>
2020-10-02 22:05:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import pyttsx3
 import datetime
 import sys
 from time import sleep as wait
-import webbrowser
-
+import webbrowser as wb
 
 def intro():
     print('=============================================================================================')
@@ -17,8 +16,12 @@ def intro():
     start()
 
 def restart():
+    print('.')
     wait(0.2)
     start()
+
+def desligar():
+    sys.exit()
 
 def reboot():
     wait(0.2)
@@ -28,6 +31,7 @@ def say(tosay):
     engine = pyttsx3.init()
     engine.say(tosay)
     engine.runAndWait()
+
 
 def start():
     while True:
@@ -39,45 +43,60 @@ def start():
             text = textc.lower()
             print(text)
             try:
+                engine = pyttsx3.init()
                 #função boas vindas
-                if text == str(name):
+                if text == str(name): #esse ouve o próprio nome dela e responde com um bom dia ou algo do tipo
                     msg_boas_vindas = choice(l_boas_vindas)
                     say(msg_boas_vindas)
                 
                 #função tocar música
-                elif text == str('tocar minha playlist'):
-                    webbrowser.open(playlist, new=2)
+                elif 'playlist' in text:#abre a playlist de muscia do usuário, pre definida em "configs"
+                    wb.open(playlist, new=2)
                 
                 #função dia
-                elif text == str('que dia é hoje'):
+                elif 'dia' in text: #fala o dia
                     print(dia)
                     say(dia)
 
                 #função horas
-                elif text == str('que horas são'):
+                elif 'horas' in text: #fala as horas
                         print(hr)
                         say(hr)
 
 			    #função piadas
-                elif text == str('piada'):
+                elif 'piada' in text: # lança a braba
                     joke = (choice(l_piadas))
                     joke = choice(l_piadas)
                     print (joke)
                     say(joke)
 
                 #função desligar
-                elif text == str('desligar sistema'):
+                elif 'desligar' in text: #desliga o sistema
                     desligando = str('desligando em 3, 2, 1')
                     print (desligando)
-                    sys.exit()
+                    engine.say(desligando)
+                    engine.runAndWait()
+                    desligar()
 
                 #função reiniciar
-                elif text == str('reiniciar sistema'):
+                elif 'reiniciar' in text: #reinicia o sistema
                     reiniciando = str('reiniciando em 3, 2, 1')
                     print (reiniciando)
+                    engine.say(reiniciando)
+                    engine.runAndWait()
                     reboot()
-				    
 
+                elif 'fale' in text:
+                    texto_falar = text.replace('fale', '')
+                    say(texto_falar)
+
+                elif 'pesquis' in text:
+                    site_pesquisar = text.replace('pesquis', '')
+                    say('pesquisando ' + site_pesquisar)
+                    wb.open('https://www.google.com/search?client=opera-gx&hs=5GZ&sxsrf=ALeKk02LWQxX_lhfnlTF6lCi_LYm0x5kqg%3A1601686367378&ei=X8t3X_LeFpPA5OUP0e6-WA&q={}&oq={}&gs_lcp=CgZwc3ktYWIQAzIHCAAQChDLATIECAAQHjoHCCMQ6gIQJzoECCMQJzoFCAAQsQM6CAguELEDEIMBOgIIADoFCC4QsQM6BAgAEAo6BggAEAoQHlD_EVjVH2COImgBcAB4AIABsgKIAdMJkgEHMC41LjAuMZgBAKABAaoBB2d3cy13aXqwAQrAAQE&sclient=psy-ab&ved=0ahUKEwiyiuDXmpfsAhUTILkGHVG3DwsQ4dUDCAw&uact=5'.format(site_pesquisar, site_pesquisar), new=2)
+                
+                elif text not in comandos:
+                    restart()
             except:
                 restart()
 intro()
